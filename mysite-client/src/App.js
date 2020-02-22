9f53786520c52b7cef611eb01fbfe7453ca180d2
Janitha Tennakoon <janitha000@gmail.com>
2020-02-22 19:45:47 +0800

    Added auth routes

--- mysite-client/src/App.js
+++ mysite-client/src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import Header from './Header/Header'
+import Login from './Auth/Login'
+import Logout from './Auth/Logout'
+import SignUp from './Auth/Signup'
 
 function App() {
   return (
     <div >
       <Router>
+        
         <Header />
+        <Route path='/login' component={Login} />
+        <Route path='/logout' component={Logout} />
+        <Route path='/signup' component={SignUp} />
       </Router>
 
     </div>
