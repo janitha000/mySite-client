9b5f09ca4124d298e77e5bb13a9971ea41874983
Janitha Tennakoon <janitha000@gmail.com>
2020-02-22 19:37:29 +0800

    Added header component

---
+++ mysite-client/src/Header/Header.js
@@ -0,0 +1,31 @@
+import React, { Fragment } from 'react'
+import { LinkContainer } from 'react-router-bootstrap'
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+function Header() {
+
+    return (
+        <Navbar bg="light" expand="lg">
+            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="#link">Link</Nav.Link>
+                </Nav>
+
+          <Nav pullRight>
+            <LinkContainer to="/signup">
+              <NavItem>Signup</NavItem>
+            </LinkContainer>
+            <LinkContainer to="/login">
+              <NavItem>Login</NavItem>
+            </LinkContainer>
+          </Nav>
+        </Navbar.Collapse>
+        </Navbar>
+    )
+}
+
+export default Header;
